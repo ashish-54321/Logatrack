9256d9a73959ae2782ee961455b426b0452afd84
Ashish <115717632+ashish-54321@users.noreply.github.com>
2024-01-18 22:36:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require('axios');
 const requestIp = require('request-ip');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware to enable CORS
 app.use(cors());
@@ -76,6 +76,6 @@ app.get("/api/logtracker", async (req, res) => {
   res.json({ message: "Log details recorded successfully!" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening at http://0.0.0.0:${port}`);
 });
